938f39c946e6e7bb1b084efdb004059dcd15ac42
imanjowkar <imanjowkar99@gmail.com>
2023-06-06 23:19:53 +0330

    Add follow and unfollow system, and add next method

--- social_media/account/views.py
+++ social_media/account/views.py
@@ -9,6 +9,7 @@ from django.urls import reverse_lazy
 from home.models import Post
 from django.contrib.auth import views as auth_views
 from . import forms
+from .models import Relation
 
 
 # Create your views here.
@@ -49,6 +50,15 @@ class UserLoginView(View):
     form_class = forms.UserLoginForm
     template_name = 'account/login.html'
     
+    
+    
+    def setup(self, request, *args, **kwargs):
+        self.next = request.GET.get('next')
+        return super().setup(request, *args, **kwargs)
+    
+    
+    
+    
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated:
             messages.warning(request, 'You are already logged in', extra_tags='warning')
@@ -69,6 +79,8 @@ class UserLoginView(View):
             if user is not None:
                 login(request, user)
                 messages.success(request, f"{cd['username']} logged in successfully", extra_tags='success')
+                if self.next:
+                    return redirect(self.next)
                 return redirect('home:home')
             messages.error(request, "username or password is incorrect")
         return render(request, self.template_name, {'form': form})
@@ -76,6 +88,7 @@ class UserLoginView(View):
     
 
 class UserLogoutView(LoginRequiredMixin, View):
+    
     # login_url = '/account/login'
     def get(self, request):
         logout(request)
@@ -86,9 +99,13 @@ class UserLogoutView(LoginRequiredMixin, View):
 class UserProfileView(LoginRequiredMixin, View):
     
     def get(self, request, user_id):
+        is_following = False
         user = get_object_or_404(User,id=user_id)
         posts = user.posts.all()
-        return render(request, 'account/profile.html', {'user': user, 'posts': posts})
+        relation = Relation.objects.filter(from_user=request.user, to_user=user)
+        if relation.exists():
+            is_following = True
+        return render(request, 'account/profile.html', {'user': user, 'posts': posts, 'is_following': is_following})
 
 
 
@@ -112,4 +129,30 @@ class UserPasswordResetCompleteView(auth_views.PasswordResetCompleteView):
     
     
     
+class UserFollowView(LoginRequiredMixin,View):
+    
+    def get(self, request, user_id):
+        user = User.objects.get(id=user_id)
+        relation = Relation.objects.filter(from_user = request.user, to_user = user)
+        if relation.exists():
+            messages.error(request, "you are already following this user", extra_tags='error')
+        else:
+            Relation.objects.create(from_user=request.user, to_user=user)
+            messages.success(request, "you followed this user", extra_tags='success')
+        return redirect('account:user-profile', user.id)
+    
+    
+    
+    
+class UserUnFollowView(LoginRequiredMixin,View):
+    
+    def get(self, request, user_id):
+        user = User.objects.get(id=user_id)
+        relation = Relation.objects.filter(from_user = request.user, to_user = user)
+        if relation.exists():
+            relation.delete()
+            messages.success(request,"you unfollowed this user", extra_tags='success')
+        else:
+            messages.error('you are not following this user', extra_tags='error')
+        return redirect('account:user-profile', user.id)    
     
